39849038bf364be7116a83c8d3c426cb11cfb043
Francisco Reyes <freyes@alumnos.inf.utfsm.cl>
2016-11-10 21:09:06 -0300

    Avances en find

--- find.py
+++ find.py
@@ -2,26 +2,46 @@
 import os
 import re
 
+def multiple_replace(dict, text):
+  regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
+  return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
+
+
 def op_find(orden):
     if None != re.search("some", orden):
+        re_result = dict()
+        for word in re.findall('"(\w+)"', orden, re.IGNORECASE):
+            re_result[word] = '\033[42m' + word + '\033[0m'
+
+        for file in os.listdir(os.getcwd()):
+            if file.endswith(".txt"):
+                tmp = open(file)
+                print "------------------------------"
+                for line in tmp:
+                    print multiple_replace(re_result,line)
+                tmp.close()
+
+    elif None != re.search("exact", orden):
+        #text_temp = ""
+        #ready = False
         for file in os.listdir(os.getcwd()):
             if file.endswith(".txt"):
                 tmp = open(file)
                 for line in tmp:
-                    for word in re.findall('"(\w+)"',orden,re.IGNORECASE):
-                        #print repr('\b+str(word)+\b')
-                        #if None != re.search("\b"+str(word)+"\b",line):
-                        if None != re.search(word, line):
-                            print re.sub(word,'\033[42m' + word + '\033[0m',line)
+                    find = re.search('"(.+)"',orden).group(0)
+                    if None != re.search(find,line):
+                        print "entro"
+                        word = re.search('"(.+)"', orden).group(0)
+                        print re.sub(word, '\033[42m' + word + '\033[0m', line)
+                        #ready = True
                 tmp.close()
-
-
-    elif None != re.search("exact", orden):
-        print "Se selecciono exact"
-
+                #if ready == True:
+                    #print text_temp
     else:
         print "Error: Comando desconocido."
     return
+
+    # word_tmp += re.sub(word,'\033[42m' + word + '\033[0m',line)
 
 #    try:
 #       fh = open("testfile", "w")
