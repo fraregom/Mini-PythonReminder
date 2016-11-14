24c14ec3c87b740f2fff8294bfad9cb2b52f5756
Diego <dzamora@inf.utfsm.cl>
2016-11-13 21:15:59 -0300

    Create LISTO

--- create.py
+++ create.py
@@ -1,18 +1,8 @@
 # coding=utf-8
 import re,os,time,subprocess
 
-#<create> ::= "create" (<name> ["edit"] | <name> {<name>}) [<tags>] ["in" <route>]
-#<tags> ::= "with tags" (<tag-name> {<tag-name>})
-
-#
-
-# create "name1" "name2" "name3" with tags "tag10" "tag20" "tag30"
-# create "hola mundo" "uno" "dos" with tags "10" "20" "30" in /doc
-# create "hola mundo" edit with tags "10" "20" "30" in /home/usr/doc
-
-#  create +(?:(?P<files>(?:".+?" *)+)|(?:(?P<edit>".+?") +edit +))(?:with +tags +(?P<tags>".*" *))?(?:in +(?P<path>(?:/.+)+))?
 def create(input,PATH):
-    patternCreate = '^ *create +(?:(?P<files>(?:".+?" *)+)|(?:(?P<edit>".+?") +edit +))'
+    patternCreate = '^ *create +(?:(?:(?P<edit>".+?") *edit *)|(?P<files>(?:".+?" *)+))'
     patternOptional = '(?:with +tags +(?P<tags>".*" *))?(?:in +/(?P<path>(?:.+)+))?$'
     pattern = re.compile(patternCreate+patternOptional)
     match = pattern.match(input)
@@ -20,26 +10,19 @@ def create(input,PATH):
         files = 'files'
         path = os.getcwd()
         if match.group('edit'):
-            print "enterEdit"
             files = 'edit'
         if match.group('path'):
-            print "enterPath"
             if re.match(r"/home/.+", match.group('path')):
                 path = match.group('path')
-                print "ruta absoluta"
             else:
                 path = os.getcwd()+'/'+match.group('path')
-                print "ruta relativa"
-            print path
             if not os.path.exists(path):
-                print "explocioon!!"
+                print "Error: Incorrect Path"
                 return
-        #print "si explota no deberia ver esto"
         ls = match.group(files)
-        #print PATH+"/metadata"
+
         metadata = open(PATH+"/metadata","a")
         for i in range(1,len(ls.split('"')),2):
-            #print path+"/"+ls.split('"')[i]+".txt"
             archivo = open(path+"/"+ls.split('"')[i]+".txt","w")
             archivo.close()
             date = time.strftime("%d/%m/%Y-%H:%M:%S")
@@ -53,12 +36,7 @@ def create(input,PATH):
             metadata.write(meta)
         metadata.close()
         if files == "edit": #comando EDIT
-            print "raw 1:"
-            raw_input()
-            subprocess.call(['nano', path + match.group(files) + '.txt'])
-            print "raw 2:"
-            raw_input()
-        print '\n-----------\n'
+            subprocess.call(['nano', path+"/"+ match.group(files).strip('"') + '.txt'])
     else:
-        print "Comando invalido\n"
+        print "Error: Incorrect Command\n"
 
