d28b25169a0348da89e245a7e1ab7006899faf68
diego ignacio zamora gajardo <dzamora@alumnos.inf.utfsm.cl>
2016-11-15 19:54:41 -0300

    RE show

--- show.py
+++ show.py
@@ -1,3 +1,8 @@
+# coding=utf-8
+import os
+import re
+
+
 # show "hola mundo"
 # show "hola mundo" in /home/cosa
 # show all
@@ -8,3 +13,13 @@
 # show all where tag is "taaaags"
 # show all where tag is "taaaags" sorted by modified
 # show all where name is "hola mundo" sorted by tags  in /home/cosa
+
+# *show +(?:"(?P<name_file>.*)"|(?P<all>all(?: +where(?:(?: +name(?: +is +"(?P<is>.*?)"| +contains +"(?P<contains>.*?)"))|(?: +tag +is +"(?P<tag>.+?)")))?)(?: +sorted +by +(?P<sort>(?:names|tags|modified|creation)))?)(:? +in +/(?P<route>(.*))/*)?
+
+def show(order,path_origin):
+    regex = re.compile(r' *show +(?:"(?P<name_file>.*)"|'
+                       r'(?P<all>all(?: +where(?:(?: +name(?: +is +"(?P<is>.*?)"|'
+                       r' +contains +"(?P<contains>.*?)"))|(?: +tag +is +"(?P<tag>.+?)")))?)'
+                       r'(?: +sorted +by +(?P<sort>(?:names|tags|modified|creation)))?)'
+                       r'(:? +in +/(?P<route>(.*))/*)?')
+    
